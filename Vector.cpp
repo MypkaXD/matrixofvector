eb3af0e5d86b997aa35b528d0d72c83d53d4d27d
MypkaXD <85798586+MypkaXD@users.noreply.github.com>
2022-04-12 14:20:17 +0300

    Update Vector.cpp

--- Vector.cpp
+++ Vector.cpp
@@ -1,154 +1,159 @@
 #include "Vector.h"
 #include <iostream>
 
-	MyVector::MyVector()
+MyVector::MyVector()
+{
+	data = nullptr;
+	N = 0;
+}
+MyVector::MyVector(int n)
+{
+	N = n;
+	data = new double[N];
+	for (int i = 0; i < N; i++)
 	{
-		data = nullptr;
-		N = 0;	
+		data[i] = 0.0;
 	}
-	MyVector::MyVector(int n)
+}
+MyVector::~MyVector()
+{
+	delete[] data;
+}
+
+
+
+void MyVector::resize(int newSize)
+{
+	double* olddata = new double[newSize];
+	int minSize = std::min(N, newSize);
+
+	for (int i = 0; i < minSize; i++)
 	{
-		N = n;
-		data = new double[N];
-		for (int i = 0; i < N; i++)
-		{
-			data[i] = 0.0;
-		}
-	}
-	MyVector::~MyVector()
-	{
-		delete[] data;
-	}
-
-	
-
-	void MyVector::resize(int newSize)
-	{
-		double* olddata = new double[newSize];
-		int minSize = std::min(N, newSize);
-
-		for (int i = 0; i < minSize; i++)
-		{
-			olddata[i] = data[i];
-		}
-
-		delete[] data;
-		data = olddata;
-
-		for (int i = minSize; i < newSize; i++)
-		{
-			data[i] = 0.0;
-		}
-
-		N = newSize;
-		
+		olddata[i] = data[i];
 	}
 
-	MyVector MyVector::operator + (const MyVector& other) const
+	delete[] data;
+	data = olddata;
+
+	for (int i = minSize; i < newSize; i++)
 	{
-		if (other.N != N) throw "ERROR";
-
-		MyVector ans(N);
-
-		for (int i = 0; i < N; i++)
-		{
-			ans[i] = data[i] + other[i];
-		}
-		return ans;
-	}
-	MyVector MyVector::operator - (const MyVector& other) const
-	{
-		if (other.N != N) throw "ERROR";
-
-		MyVector ans(N);
-
-		for (int i = 0; i < N; i++)
-		{
-			ans[i] = data[i] - other[i];
-		}
-		return ans;
+		data[i] = 0.0;
 	}
 
-	MyVector MyVector::operator * (double c) const
+	N = newSize;
+
+}
+
+MyVector MyVector::operator + (const MyVector& other) const
+{
+	if (other.N != N) throw "ERROR";
+
+	MyVector ans(N);
+
+	for (int i = 0; i < N; i++)
 	{
-		MyVector ans(N);
-		for (int i = 0; i < N; i++)
-		{
-			ans[i] = data[i] * c;
-		}
-		return ans;
+		ans[i] = data[i] + other[i];
 	}
+	return ans;
+}
+MyVector MyVector::operator - (const MyVector& other) const
+{
+	if (other.N != N) throw "ERROR";
 
-	MyVector& MyVector::operator *= (double c)
+	MyVector ans(N);
+
+	for (int i = 0; i < N; i++)
 	{
-		for (int i = 0; i < N; i++)
-		{
-			data[i] *= c;
-		}
-		return *this;
+		ans[i] = data[i] - other[i];
 	}
+	return ans;
+}
 
-	MyVector::MyVector(const MyVector& other)
+MyVector MyVector::operator * (double c) const
+{
+	MyVector ans(N);
+	for (int i = 0; i < N; i++)
 	{
-		resize(other.N);
-
-		for (int i = 0; i < N; i++)
-		{
-			data[i] = other[i];
-		}
+		ans[i] = data[i] * c;
 	}
+	return ans;
+}
 
-	MyVector& MyVector::operator=(const MyVector& other)
+MyVector& MyVector::operator *= (double c)
+{
+	for (int i = 0; i < N; i++)
 	{
-		resize(other.N);
-		for (int i = 0; i < N; i++)
-		{
-			data[i] = other[i];
-		}
-		return *this;
+		data[i] *= c;
 	}
+	return *this;
+}
 
-	MyVector& MyVector::operator+=(const MyVector& other)
+MyVector::MyVector(const MyVector& other)
+{
+	resize(other.N);
+
+	for (int i = 0; i < N; i++)
 	{
-		if (other.N != N) throw "ERROR";
-		for (int i = 0; i < N; i++)
-		{
-			data[i] += other[i];
-		}
-		return *this;
+		data[i] = other[i];
 	}
+}
 
-	MyVector& MyVector::operator-=(const MyVector& other)
+MyVector& MyVector::operator=(const MyVector& other)
+{
+	resize(other.N);
+	for (int i = 0; i < N; i++)
 	{
-		if (other.N != N) throw "ERROR";
-		for (int i = 0; i < N; i++)
-		{
-			data[i] -= other[i];
-		}
-		return *this;
+		data[i] = other[i];
 	}
+	return *this;
+}
 
-
-	double MyVector::dot(const MyVector& other)	const
+MyVector& MyVector::operator+=(const MyVector& other)
+{
+	if (other.N != N) throw "ERROR";
+	for (int i = 0; i < N; i++)
 	{
-		double ans = .0;
-		for (size_t i = 0; i < N; i++)
-		{
-			ans += other[i] * data[i];
-		}
-		return ans;
+		data[i] += other[i];
 	}
+	return *this;
+}
 
-	std::ostream& operator<<(std::ostream& out, const MyVector& v)
+MyVector& MyVector::operator-=(const MyVector& other)
+{
+	if (other.N != N) throw "ERROR";
+	for (int i = 0; i < N; i++)
 	{
-		out << "{ ";
-		for (int i = 0; i < v.N; i++)
-		{
-			std::cout << v.data[i] << " ";
-		}
-		out << "}";
-		return out;
+		data[i] -= other[i];
 	}
+	return *this;
+}
 
+
+double MyVector::dot(const MyVector& other)	const
+{
+	double ans = .0;
+	for (size_t i = 0; i < N; i++)
+	{
+		ans += other[i] * data[i];
+	}
+	return ans;
+}
+
+std::ostream& operator<<(std::ostream& out, const MyVector& v)
+{
+	out << "{ ";
+	for (int i = 0; i < v.N; i++)
+	{
+		std::cout << v.data[i] << " ";
+	}
+	out << "}";
+	return out;
+}
+/*
+MyVector MyVector::arrayofvector()
+{
+	double arrayofvector = new double
+}
+*/
 
 
